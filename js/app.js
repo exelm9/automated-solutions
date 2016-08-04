844f34337cb3a720b037640086d475b2a31acd73
exelm9 <erikmildner87@gmail.com>
2016-08-04 14:54:59 -0700

    adds showing and hiding of files

--- js/app.js
+++ js/app.js
@@ -4,6 +4,8 @@
   $('.billPayments').on('click', function(e){
     e.preventDefault();
     $('.routes').hide();
+    $('#chatModule').css({'height': '623px'})
+    $('.preChatSurvey').removeClass('hidden')
   })
 
 })();
